f92072252fce989158d7d04646745d02b169cc82
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-11 16:45:31 +0100

    tags links

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -14,12 +14,24 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
-    redirect('/links')
+    link = Link.new(url: params[:url],
+                    title: params[:title])
+    tag = Tag.create(name: params[:tag])
+    link.tags << tag
+    link.save
+    # Link.create(url: params[:url], title: params[:title])
+    redirect to('/links')
   end
 
   get '/links/new' do
     erb :'links/new'
+  end
+
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,7 +1,26 @@
 feature 'Viewing links' do
 
+  before(:each) do
+  Link.create(url: 'http://www.makersacademy.com',
+              title: 'Makers Academy',
+              tags: [Tag.first_or_create(name: 'education')])
+  Link.create(url: 'http://www.google.com',
+              title: 'Google',
+              tags: [Tag.first_or_create(name: 'search')])
+  Link.create(url: 'http://www.zombo.com',
+              title: 'This is Zombocom',
+              tags: [Tag.first_or_create(name: 'bubbles')])
+  Link.create(url: 'http://www.bubble-bobble.com',
+              title: 'Bubble Bobble',
+              tags: [Tag.first_or_create(name: 'bubbles')])
+end
+
+
+
+
+
   scenario 'I can see existing links on the links page' do
-    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    # Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
 
     visit '/links'
     puts current_path
@@ -14,6 +33,20 @@ feature 'Viewing links' do
     # might we get a false positive if we just test for 'Makers Academy'?
     within 'ul#links' do
       expect(page).to have_content('Makers Academy')
+    end
+  end
+
+
+
+
+
+  scenario 'I can filter links by tag' do
+    visit '/tags/bubbles'
+    within 'ul#links' do
+      expect(page).not_to have_content('Makers Academy')
+      expect(page).not_to have_content('Code.org')
+      expect(page).to have_content('This is Zombocom')
+      expect(page).to have_content('Bubble Bobble')
     end
   end
 end
